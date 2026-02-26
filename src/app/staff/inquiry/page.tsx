0eaa0fd408233f4fcb7fb775f9ab6a6055194b44
hokhoi1979 <166026796+hokhoi1979@users.noreply.github.com>
2026-02-26 16:20:18 +0700

    feat: add new table in staff/inquiry

--- src/app/staff/inquiry/page.tsx
+++ src/app/staff/inquiry/page.tsx
@@ -28,9 +28,66 @@ UPDATE STATUS FOR INQUIRY:
 - Closed
 */
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontalIcon } from "lucide-react";
 
 export default function InquiryPageStaff() {
-     return (
-          <div>INQUIRY PAGE</div>
-     )
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Inquiries</TableHead>
+          <TableHead>Current Tenant</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>A101</TableCell>
+          <TableCell className="font-medium">BS16 Vinhome GrandPark</TableCell>
+          <TableCell>$29.99</TableCell>
+          <TableCell>12</TableCell>
+          <TableCell>Nguyễn Văn A</TableCell>
+          <TableCell>Available</TableCell>
+          <TableCell className="text-right">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="size-8">
+                  <MoreHorizontalIcon />
+                  <span className="sr-only">Open menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>View detail</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem variant="destructive">
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
 }
