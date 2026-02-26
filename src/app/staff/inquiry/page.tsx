d3a66ccb52a08f4f99efc3d86fdda00cd8009963
Bao Trong <nhbaotrong@gmail.com>
2026-02-26 08:45:37 +0700

    chore: note for page

--- src/app/staff/inquiry/page.tsx
+++ src/app/staff/inquiry/page.tsx
@@ -1,3 +1,33 @@
+/*
+TABLE:
+- ID
+- Customer Name
+- Phone
+- Apartment
+- Status
+- Created At
+- Last activity
+- Action
+
+INQUIRY DETAIL:
+- Customer: 
+     + Name
+     + Phone
+     + Email
+     + Note
+- Apartment: 
+     + Name
+     + Price
+     + Status
+     + Button view detail
+
+UPDATE STATUS FOR INQUIRY:
+- Contacted
+- Interedted / Not 
+- Scheduled Viewing
+- Closed
+*/
+
 
 export default function Inquiry() {
      return (
