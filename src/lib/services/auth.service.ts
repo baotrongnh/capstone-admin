4296c216ca6cf2de8b4c5bd4bd245c9125f6d074
Aries <lehuy25032004@gmail.com>
2026-03-27 14:51:40 +0700

    feat(auth): add login for staff and operator role, add navigation

--- src/lib/services/auth.service.ts
+++ src/lib/services/auth.service.ts
@@ -1,14 +1,26 @@
 import { LoginDTO, LoginPayload, LoginRes, LogoutDTO, LogoutRes } from "@/types/auth";
 import { apiClient } from "../apis/client";
 import { endpoints } from "../apis/endpoints";
+import { message } from "antd";
+import { useTranslations } from "next-intl";
 
 export const authServices = {
     login: async (payload: LoginDTO): Promise<LoginPayload> => {
-        const { data } = await apiClient.post(`${endpoints.auth}/login`, payload)
-        return data.data
+        try {
+            const { data } = await apiClient.post(`${endpoints.auth}/login`, payload);
+            return data.data;
+        } catch (error) {
+            message.error('Có lỗi đã xảy ra!')
+            throw error;
+        }
     },
     logout: async (refreshToken: LogoutDTO): Promise<LogoutRes> => {
-        const { data } = await apiClient.post(`${endpoints.auth}/logout`, refreshToken)
-        return data
+        try {
+            const { data } = await apiClient.post(`${endpoints.auth}/logout`, refreshToken);
+            return data;
+        } catch (error) {
+            message.error('Có lỗi đã xảy ra!')
+            throw error;
+        }
     }
 }
