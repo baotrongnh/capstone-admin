865095db405f029b324a15a676c5e876ec531f12
Bao Trong <nhbaotrong@gmail.com>
2026-03-30 19:14:09 +0700

    chore: update sidebar staff

--- src/components/layout/staff/staff-nav-main.tsx
+++ src/components/layout/staff/staff-nav-main.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import { IconBell, IconCirclePlusFilled, type Icon } from "@tabler/icons-react"
+import { type Icon } from "@tabler/icons-react"
 
 import { isSidebarItemActive } from "@/components/layout/sidebar-active"
-import { Button } from "@/components/ui/button"
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -16,39 +15,21 @@ import { usePathname } from "next/navigation"
 
 export function StaffNavMain({
   items,
-  quickContractLabel = "Tạo hợp đồng nhanh",
+  groupLable = "groupLable",
 }: {
   items: {
     title: string
     url: string
     icon?: Icon
   }[]
-  quickContractLabel?: string
+  groupLable?: string
 }) {
   const pathname = usePathname()
 
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip={quickContractLabel}
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>{quickContractLabel}</span>
-            </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0"
-              variant="outline"
-            >
-              <IconBell />
-              <span className="sr-only">Thông báo</span>
-            </Button>
-          </SidebarMenuItem>
-        </SidebarMenu>
+        <span className="text-muted-foreground text-[12px]">{groupLable}</span>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
